0a8c22e8a5f43a31100f5f308c0fb13fdd6f14ff
Will <will@willacton.com>
2016-12-11 11:56:46 -0800

    styling

--- src/view.tsx
+++ src/view.tsx
@@ -28,7 +28,7 @@ const SearchInput = observeComponent<React.HTMLProps<any>>(
 function View({ value, results = [], showResults = false, highlighted = null }: ViewProps) {
 	console.log(value, results, showResults);
 	return (
-		<div>
+		<div style={styles.comboBox}>
 			<SearchInput style={styles.search} type="text" value={value} />
 			{showResults ?
 				<ResultsList
@@ -47,40 +47,56 @@ const ResultsList =
 		'onMouseEnter',
 		'onMouseLeave',
 	)(({ results, onClick, onMouseEnter, onMouseLeave, highlighted }: ResultsListProps) => (
-		<ul style={styles.resultsBox}>
-			{results.map((title: string, i: number) => 
-				<li
-					key={i}
-					onClick={() => onClick({ title })}
-					onMouseEnter={() => onMouseEnter(i)}
-					style={Object.assign(
-						{},
-						styles.result,
-						i === highlighted ? styles.highlighted : {},
-					)}
-					onMouseLeave={() => onMouseLeave()}
-				>
-					{title}
-				</li>
-			)}
-		</ul>
+		<div style={styles.resultsBox}>
+			<ul style={styles.resultsList}>
+				{results.map((title: string, i: number) => 
+					<li
+						key={i}
+						onClick={() => onClick({ title })}
+						onMouseEnter={() => onMouseEnter(i)}
+						style={Object.assign(
+							{},
+							styles.result,
+							i === highlighted ? styles.highlighted : {},
+						)}
+						onMouseLeave={() => onMouseLeave()}
+					>
+						{title}
+					</li>
+				)}
+			</ul>
+		</div>
 	));
 
 const styles = {
+	comboBox: {
+		position: 'relative',
+	},
 	search: {
-		fontSize: 20,
+		fontSize: 15,
 		width: "100%",
-		fontFamily: "sans-serif"
+		fontFamily: "sans-serif",
+		height: 30,
 	},
 	resultsBox: {
+		position: 'absolute',
+		top: 30,
+		left: 0,
+		right: 0,
+		zIndex: 999,
+		backgroundColor: '#fff',
+		boxShadow: "0px 4px 4px rgb(220,220,220)",
+	},
+	resultsList: {
 		listStyle: "none",
 		padding: 0,
 		margin: 0,
+		border: "1px solid #ccc",
 	},
 	result: {
 		fontFamily: "sans-serif",
 		fontSize: 16,
-		border: "1px solid #d3d3d3",
+		borderBottom: "1px solid #ccc",
 		padding: 5,
 	},
 	highlighted: {
